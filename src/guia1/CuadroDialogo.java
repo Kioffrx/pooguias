2337b0a14e092143e87289368a65b0db2a17f3dd
Kioffrx <entiquezometa@gmail.com>
2025-03-01 21:05:37 -0600

    Delete src directory

--- src/guia1/CuadroDialogo.java
+++
@@ -1,9 +0,0 @@
-package guia1;
-import javax.swing.JOptionPane;
-
-public class CuadroDialogo {
-    public static void main(String[] args){
-        JOptionPane.showMessageDialog(null, "Bienvenido a IntelliJ IDEA IDE 2020");
-        System.exit(0);
-    }
-}
